8819de9af653e123b77feaa923d19bc291c98a39
mangsgi <aodrntjr0901@naver.com>
2024-12-08 21:59:23 +0900

    test: revise initial database

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 secrets = toml.load("../secrets.toml")
 
-engine = create_engine(secrets['DATABASE_URL_LOCAL']) # 데이터베이스와 연결 생성
+engine = create_engine(secrets['DATABASE_URL']) # 데이터베이스와 연결 생성
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # 데이터베이스와의 세션 생성
 
 Base = declarative_base() # ORM을 위한 베이스 클래스, 테이블 모델 정의 시 이 클래스 상속
